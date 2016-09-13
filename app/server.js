b1bdf6b167d5fff8b2a6d87c51bf36c044c5f9b7
Blake <blake@isolary.com>
2016-09-13 13:56:22 -0700

    Moved around the including of EJS and fixed typo

--- app/server.js
+++ app/server.js
@@ -1,24 +1,29 @@
 // node application setup
 
 
-var express = require('express');
-var app = express();
+var express    = require('express');
+var app        = express();
 expressLayouts = require('express-ejs-layouts');
+var path       = require('path');
+
 
 app.port = process.env.PORT || 8000;
-
-// Bring in all routes
-var router = require('./routes');
-
-// middleware for all routes
-app.use('/',router);
 
 // template engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+// Bring in all routes
+var router = require('./routes');
+
+// middleware for all routes
+app.use('/', router);
+
+
+
 // set views directory
 app.set('views', __dirname + '/../public/views');
+console.log(__dirname + '/../public/views');
 
 /**
  * By default Express will not serve static files. It needs to be told explicitly 
