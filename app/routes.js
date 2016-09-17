b6b669a279775b65e74367600ef60ce4bc9acc13
Haani Niyaz <haani.niyaz@gmail.com>
2016-09-17 10:07:10 +1000

    Add evetns show route

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@
 
 // busines logic
 mainController = require('./controllers/mainController');
+eventsController = require('./controllers/eventsController')
 
 // create a router
 var router = require('express').Router();
@@ -9,6 +10,7 @@ var router = require('express').Router();
 
  // plug in controller to route
 router.get('/', mainController.home);
+router.get('/events', eventsController.show);
 
 // export router
 module.exports = router;
