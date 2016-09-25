eca8b956bacd607872a9b7c6691c40663f9f2a82
Haani Niyaz <haani.niyaz@gmail.com>
2016-09-25 17:12:48 +1000

    Load depedencies for sessions and cookies

--- app/server.js
+++ app/server.js
@@ -2,13 +2,37 @@
 
 require('dotenv').config();
 
-var express = require('express');
-var app = express();
-var expressLayouts = require('express-ejs-layouts');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var express 		= require('express');
+var app 			= express();
+var expressLayouts 	= require('express-ejs-layouts');
+var mongoose     	= require('mongoose');
+var bodyParser 	 	= require('body-parser');
+
+
+/**
+ * express-session: gives us the ability to create sessions in Node so that we can store data.
+ * cookie-parser  : give us the ability to read those sessions that are stored in cookies.
+ * connect-flash  : let us save messages to flash-data (one-time use messages) 
+ * 				    which we'll display as success/error messages.
+ */
+
+var session      	= require('express-session'),
+  cookieParser   	= require('cookie-parser'),
+  flash          	= require('connect-flash');
 
 app.port = process.env.PORT || 8000;
+
+
+app.use(cookieParser());
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	cookie: {maxAge: 60000},
+	resave: false, // forces the session to be saved back to the store
+	saveUninitialized: false // don't save unmodified sessions
+}));
+
+
+app.use(flash());
 
 // load all routes
 var router = require('./routes');
